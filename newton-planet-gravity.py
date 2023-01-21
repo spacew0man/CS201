0fa822807b34063841991f757addd259114d1917
spacew0man <40811076+spacew0man@users.noreply.github.com>
2023-01-21 14:52:31 -0500

    Update newton-planet-gravity.py

--- newton-planet-gravity.py
+++ newton-planet-gravity.py
@@ -1,7 +1,7 @@
-print('What is the mass of the planet?')
+print('What is the mass of the planet, in kg?')
 planet_mass = float(input())
-print('What is the radius of the planet?')
+print('What is the radius of the planet, in kg?')
 planet_radius = float(input())
 GRAVITATIONAL_CONSTANT = 6.674E-11
 acceleration = GRAVITATIONAL_CONSTANT * planet_mass / (planet_radius ** 2)
-print('The acceleration is ' + str(acceleration))
+print('The acceleration is ' + str(acceleration) + ' m/s**2')
